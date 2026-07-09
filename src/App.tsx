446157553e5fb079dc1868bcb9e874f4f63ea250
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-07-09 11:15:42 +0545

    initialize project, clean up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='min-h-screen'>
+      <h1 className='text-5xl text-primary'>Hello World</h1>
+      <p className='text-secondary'>Hello hello</p>
+    </div>
+  )
+}
+
+export default App
