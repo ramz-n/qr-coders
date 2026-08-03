26d0501c39898d5eca4f774358095f1934a1585a
MahamadShah <qaiumshah@yahoo.com>
2026-08-03 12:59:16 +0545

    All logos are changed with our website qr code and favicon changed too

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer id="footer" className='border-gray-800 border-t-2'>
             <div className="mx-auto max-w-7xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
                 <div className="flex md:items-center flex-col md:flex-row justify-between gap-10">
-                    <img src="./qrc-logo-white.png" alt="qrc logo" className="h-20 w-fit" />
+                    <img src="./logo-white.png" alt="qrc logo" className="h-20 w-fit" />
                     <div>
                         <h2 className="text-sm font-semibold uppercase tracking-wider text-white">
                             Company
